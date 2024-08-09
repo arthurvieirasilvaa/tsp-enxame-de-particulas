d0d6df69f80f4e25ce752103b5bf12c928d933a0
Arthur Vieira Silva <113110861+arthurvieirasilvaa@users.noreply.github.com>
2024-08-09 16:09:56 -0300

    Finalizei o projeto

--- tsp-pso.py
+++ tsp-pso.py
@@ -69,12 +69,13 @@ def inicializar_particulas(N, numero_cidades):
     return particulas
                   
                   
-def inicializar_velocidades(numero_cidades):
-    """"""
+def inicializar_velocidades(N, numero_cidades):
+    """
+    Função utilizada para inicializar a lista de velocidades na qual, cada 
+    posição, terá uma lista de tuplas de pares (i, j), onde i e j são os 
+    índices dos elementos (cidades) da partícula que serão trocados:
+    """
 
-    # A velocidade será uma lista na qual, cada posição, terá uma lista de
-    # tuplas de pares (i, j), onde i e j são os índices dos elementos
-    # (cidades) da partícula que serão trocados:
     velocidade = []
     
     permutacoes = list(itertools.permutations(range(numero_cidades), 2))
@@ -86,7 +87,7 @@ def inicializar_velocidades(numero_cidades):
             
     permutacoes_removidas = list(permutacoes_removidas)
     
-    for _ in range(numero_cidades):
+    for _ in range(N):
         random.shuffle(permutacoes_removidas)
         velocidade.append(permutacoes_removidas)
         
@@ -109,70 +110,59 @@ def fitness(Grafo, rota, numero_cidades):
     
     return distancia
 
-def atualizar_velocidade(rota, nova_rota, velocidade):
+        
+def atualizar_particula(particula, numero_cidades, velocidade, w, c1, c2, pBest, gBest):
     """
-    Atualiza o vetor de velocidade de uma determinada partícula adicionando as
-    tuplas de índices que deverão ser trocados.
+    Função utilizada para atualizar uma determinada partícula, realizando
+    trocas nos índices das cidades e atualizando a posição da partícula.
     """
     
-    copia_rota = rota[:] # representa uma cópia da rota atual da partícula.
+    solucao_particula = particula[:]
+    solucao_pBest = pBest[:]
+    solucao_gBest = gBest[:]
+    temp_velocidade = []
     
-    for i in range(len(copia_rota)):
-        # Verificamos se a cidade está ou não na posição correta:
-        if copia_rota[i] != nova_rota[i]:
-            indice_troca = nova_rota.index(copia_rota[i])
-            velocidade.append((i, indice_troca))
+    # inércia (manter parte da velocidade atual):
+    temp_velocidade = [(i, j) for (i, j) in velocidade if random.random() < w]
+
+    # Componente cognitiva (relação do pbest):
+    for i in range(numero_cidades):
+        if solucao_particula[i] != solucao_pBest[i] and random.random() < c1:
+            operador_troca = (i, solucao_pBest.index(solucao_particula[i]))
+            temp_velocidade.append(operador_troca)
             
-    return velocidade
+            aux = solucao_pBest[operador_troca[0]]
+            solucao_pBest[operador_troca[0]] = solucao_pBest[operador_troca[1]]
+            solucao_pBest[operador_troca[1]] = aux
 
-
-def realizar_troca(rota, velocidade):
-    """
-    Atualiza o vetor de velocidade de uma determinada partícula adicionando as
-    tuplas de índices que deverão ser trocados.
-    """
+    # Componente social (relação do gbest):
+    for i in range(numero_cidades):
+        if solucao_particula[i] != solucao_gBest[i] and random.random() < c2:
+            operador_troca = (i, solucao_gBest.index(solucao_particula[i]))
+            temp_velocidade.append(operador_troca)
+            
+            aux = solucao_gBest[operador_troca[0]]
+            solucao_gBest[operador_troca[0]] = solucao_gBest[operador_troca[1]]
+            solucao_gBest[operador_troca[1]] = aux
+        
+    velocidade = temp_velocidade[:]
     
     for (i, j) in velocidade:
-        rota[i] = rota[j]
-        rota[j] = rota[i]
-
+        aux = solucao_particula[i]
+        solucao_particula[i] = solucao_particula[j]
+        solucao_particula[j] = aux
         
-def atualizar_particula(particula, N, velocidade, w, c1, c2, pBest, gBest):
-    """"""
-        
-    pBest_diferenca = gBest_diferenca = []
+    return solucao_particula
     
-    for i in range(N):
-        pBest_diferenca.append(pBest[i] - particula[i])
-        gBest_diferenca.append(gBest[i] - particula[i])
-        
-    pBest_diferenca = np.array(pBest_diferenca)
-    gBest_diferenca = np.array(gBest_diferenca)
-                
-    for v in velocidade:
-        
-        r1 = random.random()
-        r2 = random.random()
-        
-        v1 = (w * v[0]) + (c1 * r1 * pBest_diferenca) + (c2 * r2 * gBest_diferenca)
-        v2 = (w * v[1]) + (c1 * r1 * pBest_diferenca) + (c2 * r2 * gBest_diferenca)
-
-        v = (round(v1), round(v2))
-    
-    print("Velocidade atualizada:")
-    print(velocidade)
-    
-    nova_particula = particula[:]
-    realizar_troca(nova_particula, velocidade)
-    for i in range(len(particula)):
-        particula[i] = particula[i] + nova_particula[i]
-                
-                
+                            
 def pso(N, M, w, c1, c2):
-    """"""
+    """
+    Algoritmo Particle Swarm Optmization (PSO) para solucionar o problema do
+    caixeiro viajante (Travelling Salesman Problem - TSP):
+    """
     
     linhas = ler_arquivo()
-              
+    
     if linhas:
         coordenadas = []
         coordenadas, numero_cidades = obter_coordenadas(coordenadas, linhas)
@@ -182,23 +172,30 @@ def pso(N, M, w, c1, c2):
         
         particulas = inicializar_particulas(N, numero_cidades)
         print("Partículas:")
-        print(particulas)
+        for particula in particulas:
+            print(particula)
+        
+        print()
             
         # pBesti (personal best): a melhor posição encontrada pela partícula i
         pBest = particulas[:]
-        print("pbest: {}".format(pBest))
+        print("pbest inicial de cada partícula:")
         
         pBest_custo = [fitness(Grafo, pBest[i], numero_cidades) for i in range(N)]        
-        print("pbest_custo: " + str(pBest_custo))
+        for i in range(N):
+            print("pbest: {}, Custo: {}".format(pBest[i], pBest_custo[i]))
+        
+        print()
         
         # gBest (global best): a melhor posição encontrada por todas as partículas
         gBest_custo = min(pBest_custo)
-        print("gbest_custo: " + str(gBest_custo))
         gBest = pBest[pBest_custo.index(gBest_custo)]
-        print("gbest: {}".format(gBest))
+        print("gbest inicial: {}, Custo: {}".format(gBest, gBest_custo))
         
-        velocidade = inicializar_velocidades(numero_cidades)
+        print()
         
+        velocidade = inicializar_velocidades(N, numero_cidades)
+                
         for _ in range(M):
             for i in range(N):                
                 custo_atual = fitness(Grafo, particulas[i], numero_cidades)
@@ -210,18 +207,17 @@ def pso(N, M, w, c1, c2):
                 if custo_atual < gBest_custo:
                     gBest_custo = custo_atual
                     gBest = particulas[i]
-                
-                print("Velocidade:")
-                print(velocidade[i])
-                    
-                atualizar_particula(particulas[i], N, velocidade[i], w, c1, c2, pBest[i], gBest)
-                    
+                                
+                particulas[i] = atualizar_particula(particulas[i], numero_cidades, velocidade[i], w, c1, c2, pBest[i], gBest)
         
+        gBest.append(gBest[0])
         return gBest, gBest_custo
         
     else:
         print("O arquivo de entrada não foi encontrado!")
 
+
+# Parâmetros utilizados no PSO:
 
 # N é o número de partículas no enxame
 # M é o número de iterações
@@ -229,7 +225,9 @@ def pso(N, M, w, c1, c2):
 # aprendizado)
 # w é ponderação de inércia
 
-gBest, gBest_custo = pso(N=10, M=100, c1=1.5, c2=1.5, w=0.7)
+gBest, gBest_custo = pso(N=40, M=1000, w=0.7, c1=0.8, c2=0.9)
+
+print()
 
 print("A melhor rota é: {}".format(gBest))
 print("O custo da melhor rota é: {}".format(gBest_custo))
